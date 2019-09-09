f3340eec6b6b54d39feac5f0314fc2c6576322e7
Pyae Phyo Hein <pyaephyohein.info.3326@gmail.com>
2019-09-10 03:36:07 +0630

    Add files via upload

---
+++ networktool.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import os
+Cmd1 = 'speedtest' 
+print("1) speedtest")
+Cmd2 = 'ping google.com'
+print("2) Ping Test")
+Cmd3 = ('ifconfig')
+print("3) IP Address")
+Cmd4 = ('exit')
+print("4) Exit")
+num = input( "Enter Number :" )
+Cmd = int(num)
+if Cmd == 1:
+    os.system(Cmd1)
+
+if Cmd == 2:
+    os.system(Cmd2)
+
+if Cmd == 3:
+    os.system(Cmd3)
+
+if Cmd == 4:
+    os.system(Cmd4)
